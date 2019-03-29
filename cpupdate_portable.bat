0af8c863f7e009a7212c760d0f69bc645f50d889
MarkHaakman <m.p.a.haakman@student.tudelft.nl>
2019-03-29 14:56:23 +0100

    Allow multiple mods to be updated

--- cpupdate_portable.bat
+++ cpupdate_portable.bat
@@ -1,330 +1,260 @@
 @echo off
-rem (c) 2016-2018 M. Busche, elpatron@mailbox.org
-rem
-rem 
-rem                                      _       _       
-rem                                     | |     | |      
-rem            ___ _ __  _   _ _ __   __| | __ _| |_ ___ 
-rem           / __| '_ \| | | | '_ \ / _` |/ _` | __/ _ \
-rem          | (__| |_) | |_| | |_) | (_| | (_| | ||  __/
-rem           \___| .__/ \__,_| .__/ \__,_|\__,_|\__\___|
-rem               | |         | |                        
-rem               |_|         |_|                        
-rem
-rem 
-rem This script installs or updates the mod "Courseplay" for Farming Simulator 17
-rem Copy this file to a folder of your choice and run it from time to time.
-rem
-rem =============================================================================
-rem                     I M P O R T A N T  N O T I C E
-rem =============================================================================
-rem Backups will be stored as ZIP files in the subfolder .\cpbackup
-rem
-rem Have a look at the settings described below!
-rem
-rem Get updates of this script only from 
-rem https://github.com/elpatron68/cpupdate/releases
-rem
-rem Have fun!
-rem =============================================================================
-rem U S E R  S E T T I N G S
-rem =============================================================================
-rem Set deployment mode:
-rem    * "ZIPFILE" creates a ZZZ_Courseplay.zip file in your mod directory
-rem    * "DIRECTORY" copies the Courseplay as subdirectory to your mod directory
-rem Both modes work fine, you have the choice.
-rem
-set deployment="ZIPFILE"
-rem set deployment="DIRECTORY"
-rem =============================================================================
-rem Set Farming Simulator version
-set fsversion="2019"
-rem set fsversion="2017"
-rem =============================================================================
-rem If you want the command window to close after run: set autoclose="YES".
-rem Otherwise you have to hit a keystroke after the run - which enables you
-rem to see what happened.
-rem
-rem set autoclose="YES"
+:: Farming Simulator Mod Updater with Git
+::
+:: This script installs or updates mods for Farming Simulator which can be downloaded from a Git repository.
+:: Copy this file to a folder of your choice and run it from time to time.
+::
+:: Original cpupdate (version 1.5): copyright by 2016-2018 M. Busche, elpatron@mailbox.org
+:: Original code by elpatron68 can be found at https://github.com/elpatron68/cpupdate/
+::
+:: =============================================================================
+:: Backups will be stored as ZIP files in the subfolder .\fs_mod_updater_backups
+:: Have fun!
+:: =============================================================================
+:: Set Farming Simulator version
+set fsversion=2019
+:: set fsversion=2017
+:: =============================================================================
+:: If you want the command window to close after run: set autoclose="YES".
+:: Otherwise you have to hit a keystroke after the run - which enables you
+:: to see what happened.
+::
+:: set autoclose="YES"
 set autoclose="NO"
-rem =============================================================================
-rem You should replace these with the full path to the files if you use this
-rem script without the portable editions of Git and 7-Zip.
-rem
-set gitexe=".\cmd\git.exe"
-set zipexe=".\App\7-Zip\7z.exe"
-rem =============================================================================
-rem End of  U S E R  S E T T I N G S
-rem =============================================================================
-rem
-rem =============================================================================
-rem References:
-rem
-rem Colors: https://stackoverflow.com/questions/2048509/how-to-echo-with-different-colors-in-the-windows-command-line
-rem Lots of other code stolen from https://stackoverflow.com and other
-rem helpful sites.
-rem =============================================================================
+:: =============================================================================
+:: You should replace these with the full path to git.exe and 7z.exe.
+::
+set gitexe="C:\Program Files\Git\bin\git.exe"
+set zipexe="C:\Program Files\7-Zip\7z.exe"
+:: =============================================================================
+:: References:
+::
+:: Colors: https://stackoverflow.com/questions/2048509/how-to-echo-with-different-colors-in-the-windows-command-line
+:: Lots of other code stolen from https://stackoverflow.com and other helpful sites.
+::
+:: Original cpupdate 1.5: copyright by 2016-2018 M. Busche, elpatron@mailbox.org
+:: Original code by elpatron68 can be found at https://github.com/elpatron68/cpupdate/
+:: =============================================================================
 setlocal enabledelayedexpansion
 
-rem Colored text only Win10+
+:: Colored text only Win10+
 for /f "tokens=2 delims=[]" %%x in ('ver') do set WINVER=%%x
 set WINVER=%WINVER:Version =%
 if "%WINVER:~0,3%"=="10." (
-	set colored=1
+    set colored=1
 ) else (
-	set colored=0
+    set colored=0
 )
 
-rem Title
+:: Title
 if %colored% == 1 (
-	echo [97mCourseplay Beta Updatescript v1.5[0m
+    echo [97mFarming Simulator Mod Updater[0m
 ) else (
-	echo Courseplay Beta Updatescript v1.5
+    echo Farming Simulator Mod Updater
 )
-echo (c) 2016-2018 elpatron@mailbox.org
-echo .
-
-rem git.exe startable?
-echo Checking for Git...
-%gitexe% --version > NUL
-if not %errorlevel%==1 set gitok="-1"
-rem 7-Zip startable?
-echo Checking for 7-Zip...
-%zipexe% >NUL
-if %errorlevel%==9009 goto zipok="-1"
-if "%gitok%"=="-1" goto gitzipfehler
-if "%zipok%"=="-1" goto gitzipfehler
-
-echo Git and 7-Zip are ok, lets move on...
 echo.
 
-rem Write a VBS file for getting Courseplay version from moddesc.xml
+:: Check of Git and 7-Zip are accesible.
+%gitexe% --version > NUL
+if not %errorlevel%==1 set gitok="-1"
+%zipexe% >NUL
+if %errorlevel%==9009 goto zipok="-1"
+if "%gitok%"=="-1" goto gitziperror
+if "%zipok%"=="-1" goto gitziperror
+
+:: Write a VBS file for getting mod version from moddesc.xml
 > "%TEMP%\getversion.vbs" (
 echo.Dim oXml: Set oXml = CreateObject^("Microsoft.XMLDOM"^)
 echo.oXml.Load WScript.Arguments.Item^(0^)
 echo.Dim oDoc: Set oDoc = oXml.documentElement
 echo.
 echo.For Each node In oDoc.childNodes
-echo.	If node.nodeName = "version" Then
-echo.		WScript.Echo node.text
-echo.	End If
+echo.    If node.nodeName = "version" Then
+echo.        WScript.Echo node.text
+echo.    End If
 echo.Next
 echo.Set oXml = Nothing
 )
 
-rem Write a VBS file for getting modfolder from gameSettings.xml
+:: Write a VBS file for getting modfolder from gameSettings.xml
 > "%TEMP%\moddir.vbs" (
 echo.Dim oXml: Set oXml = CreateObject^("Microsoft.XMLDOM"^)
 echo.oXml.Load WScript.Arguments.Item^(0^)
 echo.Dim oDoc: Set oDoc = oXml.documentElement
 echo.
 echo.For Each node In oDoc.childNodes
-echo.	If ^(node.nodeName = "modsDirectoryOverride"^) And ^(node.getAttribute^("active"^) = "true"^) Then
-echo.		WScript.Echo node.getAttribute^("directory"^)
-echo.	End If
+echo.    If ^(node.nodeName = "modsDirectoryOverride"^) And ^(node.getAttribute^("active"^) = "true"^) Then
+echo.        WScript.Echo node.getAttribute^("directory"^)
+echo.    End If
 echo.Next
 echo.Set oXml = Nothing
 )
 
-rem Setting `Documents` folder
+:: Set `Documents` folder.
 for /f "skip=2 tokens=2*" %%A in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Personal"') do set "UserDocs=%%B"
 
-:: Setting Mod directory
+::
+:: Ask user which mod should be updated.
+::
+echo Type the name of one of the supported mods you would like to update:
+cmd /c .\includes\ini.bat /i gitRepoName mods.ini
+set /p gitRepoName="Enter name: "
+
+:: Retrieve the mod paramters from mods.ini
+for /F "tokens=* USEBACKQ" %%F IN (`.\includes\ini.bat /s %gitRepoName% /i gitUrl mods.ini`) DO (
+    set gitUrl=%%F
+)
+for /F "tokens=* USEBACKQ" %%F IN (`.\includes\ini.bat /s %gitRepoName% /i modLocationFromRepoRoot mods.ini`) DO (
+    set modLocationFromRepoRoot=%%F
+)
+set modDestinationName=FS19_%gitRepoName%
+
+:: Set mod directory.
 SETLOCAL
 for /f "delims=" %%i in ('cscript %TEMP%\moddir.vbs "%UserDocs%\My Games\FarmingSimulator%fsversion%\gameSettings.xml" //Nologo') do set moddir=%%i
 
-rem Setting Courseplay mod folder or file
+:: Set file destination.
 if defined moddir (
-	set _dest=%moddir%\ZZZ_courseplay
-	) else (
-	set _dest=%UserDocs%\my games\FarmingSimulator%fsversion%\mods\ZZZ_courseplay
-)
-
-echo Deployment method is %deployment%.
-if %deployment%=="DIRECTORY" (
-	set destination=%_dest%
+    set destination=%moddir%\%modDestinationName%.zip
 ) else (
-	set destination=%_dest%.zip
+    set destination=%UserDocs%\my games\FarmingSimulator%fsversion%\mods\%modDestinationName%.zip
 )
-echo Your destination is: %destination% 
-	
-rem Is this a fresh installation or an update?
+echo Your destination is: %destination%
+
+:: Check of this a fresh installation or an update.
 if exist "%destination%" (
-	set freshinstall="no"
-	echo Previous version found, switching to update mode.
+    set freshinstall="no"
 ) else (
-	set freshinstall="yes"
-	echo No previous version found, switching to fresh install mode..
+    set freshinstall="yes"
+    echo No previous version found, switching to fresh install mode...
 )
 
-rem Backup directory
-set "curpath=%cd%"
-set backupdir=.\cpbackup
-echo Your backup folder is: %curpath%\cpbackup
-
-rem Extract moddesc.xml from ZIPFILE
-if %deployment%=="ZIPFILE" (
-	if %freshinstall%=="no" (
-		echo Extracting 'moddesc.xml' for version detection...
-		del /q "%TEMP%\moddesc.xml" 2>NUL
-		%zipexe% e "%destination%" -o"%TEMP%" moddesc.xml -r -aoa > NUL 2>&1
-	)
+:: Extract moddesc.xml from ZIPFILE.
+if %freshinstall%=="no" (
+    del /q "%TEMP%\moddesc.xml" 2>NUL
+    %zipexe% e "%destination%" -o"%TEMP%" moddesc.xml -r -aoa > NUL 2>&1
+)
+:: Get current mod version with vbs script.
+if exist .\%gitRepoName%version.txt (
+    del /q .\%gitRepoName%version.txt 1,2>NUL
 )
 
-rem Get current Courseplay version with vbs script
-if exist .\cpversion.txt (
-	del /q .\cpversion.txt 1,2>NUL
+:: Extract moddesc.xml from zip file.
+if %freshinstall%=="no" (
+    cscript "%TEMP%\getversion.vbs" "%TEMP%\moddesc.xml" //Nologo >.\%gitRepoName%version.txt
 )
 
-rem ...from directory
-if %deployment%=="DIRECTORY" (
-	if %freshinstall%=="no" (
-		cscript "%TEMP%\getversion.vbs" "%destination%\modDesc.xml" //Nologo >.\cpversion.txt
-	)
-) else (
-rem from zip file
-	if %freshinstall%=="no" (
-		cscript "%TEMP%\getversion.vbs" "%TEMP%\moddesc.xml" //Nologo >.\cpversion.txt
-	)
-)
-
-rem sleep 2 seconds
+:: Sleep for 2 seconds.
 ping 127.0.0.1 -n 2 > nul
 
-rem Read version from output file
-if exist .\cpversion.txt (
-	set /p version=<.\cpversion.txt
-	set freshinstall="no"
-	del /q .\cpversion.txt 1,2>NUL
+:: Read version from the output file.
+if exist .\%gitRepoName%version.txt (
+    set /p version=<.\%gitRepoName%version.txt
+    set freshinstall="no"
+    del /q .\%gitRepoName%version.txt 1,2>NUL
 ) else (
-	set freshinstall="yes"
-	set version="0"
+    set freshinstall="yes"
+    set version="0"
 )
 
 if %freshinstall%=="no" (
-	if %colored% == 1 (
-		echo [42mYour currently installed Version is: %version%[0m
-	) else (
-	echo Your currently installed Version is: %version%
-	)
+    if %colored% == 1 (
+        echo [44mYour currently installed version of %gitRepoName% is: %version%[0m
+    ) else (
+        echo Your currently installed version of %gitRepoName% is: %version%
+    )
 )
 
-rem Delete old checkout
-echo Deleting temporary folder...
-rd /s/q .\courseplay 2> NUL
+:: Delete old checkout folder.
+rd /s/q .\%gitRepoName% 2> NUL
 
-rem Git clone
-echo Cloning Courseplay repository from Github...
-%gitexe% clone --depth 1 -q https://github.com/Courseplay/courseplay.git
+:: Git clone.
+echo Cloning %gitRepoName%...
+%gitexe% clone --depth 1 -q %gitUrl%
 
-rem Delete .git folder and other files not needed
-echo Deleting git files
-rd /s/q .\courseplay\.git 2> NUL
-del .\courseplay\.editorconfig /s >NUL 2>&1
-del .\courseplay\.gitattributes /s >NUL 2>&1
-del .\courseplay\.gitignore  >NUL 2>&1
-del .\courseplay\course-generator\LICENSE  >NUL 2>&1
-del .\courseplay\__Attributions.md /s  >NUL 2>&1
-del .\courseplay\readme.* /s >NUL 2>&1
-
-rem Get new Courseplay version information
-cscript "%TEMP%\getversion.vbs" ".\courseplay\modDesc.xml" //Nologo >.\cpversion.txt
-set /p newversion=<.\cpversion.txt
+:: Get new mod version information.
+cscript "%TEMP%\getversion.vbs" ".\%gitRepoName%%modLocationFromRepoRoot%modDesc.xml" //Nologo >.\%gitRepoName%version.txt
+set /p newversion=<.\%gitRepoName%version.txt
 if %colored% == 1 (
-	echo [44mVersion from Github: %newversion%[0m
+    echo [44m%gitRepoName% version from Git: %newversion%[0m
 ) else (
-	echo Version from Github: %newversion%
+    echo %gitRepoName% version from Git: %newversion%
 )
 
-if exist .\cpversion.txt (
-	del /q .\cpversion.txt 1,2>NUL
+if exist .\%gitRepoName%version.txt (
+    del /q .\%gitRepoName%version.txt 1,2>NUL
 )
 
-rem Do we have an update?
-if "%newversion%"=="%version%" (
-	if %colored% == 1 (
-		echo [103mNo update found, exiting.[0m
-	) else (
-		echo No update found, exiting.
-	)
-	rd /s/q .\courseplay 2> NUL
-	goto ende
-	) else (
-	echo We have found an update.
-)
-
-rem Backup current version
-if %deployment%=="DIRECTORY" (
-	set backupfile=%backupdir%\courseplay_backup-%version%.zip
+:: Check if there is an update.
+if "%newversion%"=="%version%" if %freshinstall%=="no" (
+    if %colored% == 1 (
+        echo [44mNo update of %gitRepoName% found, exiting.[0m
+    ) else (
+        echo No update of %gitRepoName% found, exiting.
+    )
+    rd /s/q .\%gitRepoName% 2> NUL
+    goto ende
 ) else (
-	set backupfile=%backupdir%\ZZZ_courseplay_%version%.zip
+    echo We have found an update.
 )
 
+:: Backup current version.
 if %freshinstall%=="no" (
-	echo Creating a backup of your current Courseplay...
-	mkdir %backupdir% 2> NUL
-	if %deployment%=="DIRECTORY" (
-		%zipexe% a -r "%backupfile%" "%destination%\*" >NUL 2>&1
-	) else (	
-		copy "%destination%" %backupfile% > NUL
-	)
-) else (
-	echo No former version found - this seems to be a fresh install. Creating new mod directory for Courseplay...
-	if %deployment%=="DIRECTORY" (
-		md "%destination%" > NUL
-	)
+    set "curpath=%cd%"
+    set backupdir=fs_mod_updater_backups
+    set backupfile=%backupdir%\%modDestinationName%_%version%.zip
+    echo backupdir: %backupdir%
+    echo modDestinationName: %modDestinationName%
+    echo version: %version%
+    echo backupfile: %backupfile%
+    mkdir .\%backupdir% 2> NUL
+    copy "%destination%" .\%backupfile% > NUL
+    echo Backup location of currently installed version of %gitRepoName%: "%curpath%\%backupfile%"
 )
 
-if not %backupfile%1==1 (
-	echo If you consider any problems, check your backup file: "%backupfile%"
-)
+:: Copy the cloned directory to mod folder.
+%zipexe% a -r -tzip "%destination%" .\%gitRepoName%%modLocationFromRepoRoot%\* >NUL 2>&1
 
-rem Copy cloned directory to mod folder
-if %deployment%=="DIRECTORY" (
-	echo Copying the update to your mod folder...
-	xcopy  /S /E /H /Y /C /Q ".\courseplay\*.*" "%destination%\" >NUL
+:: Delete Git clone directory.
+rd /s/q .\%gitRepoName% 2> NUL
+if %freshinstall% == "no" (
+    if %colored% == 1 (
+            echo [44mSucessfully updated %gitRepoName% from %version% to %newversion%.[0m
+        ) else (
+            echo Sucessfully updated %gitRepoName% from %version% to %newversion%.
+        )
 ) else (
-	echo Copying the updated Courseplay directory as ZIP file to your mod folder...
-	%zipexe% a -r -tzip "%destination%" .\courseplay\* >NUL 2>&1
-)
-
-rem  Delete Git clone directory
-echo Deleting temporary folder...
-rd /s/q .\courseplay 2> NUL
-if %colored% == 1 (
-	echo [102mSucessfully updated from %version% to %newversion%.[0m
-) else (
-	echo Sucessfully updated from %version% to %newversion%.
+    if %colored% == 1 (
+            echo [44mSucessfully installed %gitRepoName% version %newversion%[0m
+        ) else (
+            echo Sucessfully installed %gitRepoName% version %newversion%
+        )
 )
 goto ende
 
-:gitzipfehler
+:gitziperror
 if "%gitok%"=="-1" (
-	echo Git for Windows has to be installed and reside in PATH!
-	echo Download: https://git-scm.com/download/win
+    echo Git for Windows has to be installed and reside in PATH!
+    echo Download: https://git-scm.com/download/win
 )
 if "%zipok%"=="-1" (
-	echo 7-Zip has to be installed and reside in PATH!
-	echo Download: http://www.7-zip.org/download.html
+    echo 7-Zip has to be installed and reside in PATH!
+    echo Download: http://www.7-zip.org/download.html
 )
 goto ende
 
 :ende
-rem Cleanup
+:: Cleanup.
 if exist "%TEMP%\getversion.vbs" (
-	del /q "%TEMP%\getversion.vbs" 2>NUL
+    del /q "%TEMP%\getversion.vbs" 2>NUL
 )
 if exist "%TEMP%\moddir.vbs" (
-	del /q "%TEMP%\moddir.vbs" 2>NUL
+    del /q "%TEMP%\moddir.vbs" 2>NUL
 )
 
-rem Goodbye
+:: Close script.
 echo.
-echo You should check for an update of cpupdate from time to time:
-echo https://github.com/elpatron68/cpupdate/releases
-echo.
-echo Bye, and thanks for the fish.
 if %autoclose%=="NO" (
-	echo ^(Any key to exit^)
-	pause >NUL
+    echo ^(Press any key to exit..^)
+    pause >NUL
 )
